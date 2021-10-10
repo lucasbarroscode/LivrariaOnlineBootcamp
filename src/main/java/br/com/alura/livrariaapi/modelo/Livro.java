ee4349518c29537944fec1840f563798452258a1
Lucas Barros Bezerra <lucasbabezerra@gmail.com>
2021-10-10 19:17:49 -0300

    Mapeamento das entidades livros e autores

--- src/main/java/br/com/alura/livrariaapi/modelo/Livro.java
+++ src/main/java/br/com/alura/livrariaapi/modelo/Livro.java
@@ -2,6 +2,13 @@ package br.com.alura.livrariaapi.modelo;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +19,19 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+//tb_gestao == new tb_livros
+@Table(name = "livros")
 public class Livro {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String titulo;
 	private LocalDate dataLancamento;
-	private int numPaginas;
+	private Integer numPaginas;
+	
+	@ManyToOne
 	private Autor autor;
 	
 	
